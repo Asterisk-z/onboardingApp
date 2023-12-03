b54d0e14957619f0c194a88fa83132a0da8194a0
James C <jamesc@Jamess-MacBook-Pro.local>
2023-12-03 17:02:24 +0100

    Add role based middleware

---
+++ api/app/Http/Middleware/AuthByMultipleRoles.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Role;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthByMultipleRoles
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $roles)
+    {
+        $roles = unserialize($roles);
+
+        if (!in_array($request->user()->role_id, $roles)) {
+            return errorResponse("999", "Unauthorized Access", [], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $next($request);
+    }
+}
