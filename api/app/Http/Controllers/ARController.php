e33e50749945325a220d0ca6ab14263482f04f50
James C <jamesc@Jamess-MacBook-Pro.local>
2023-12-03 17:45:07 +0100

    Add ar

--- api/app/Http/Controllers/ARController.php
+++ api/app/Http/Controllers/ARController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Utility;
+use App\Http\Requests\AR\AddARRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ARController extends Controller
 {
-    public function addAR()
+    public function addAR(AddARRequest $request)
     {
-        return "Hi there";
+
+
+        $validated = $request->validated();
+        $validated['institution_id'] = $request->user()->institution_id;
+
+        $password = Utility::generatePassword();
+        $validated['password'] = \Hash::make($password);
+        $validated['created_by'] = $request->user()->id;
+
+
+        $user = User::create($validated);
+
+        //TODO::SEND MAIL to user and admin for approval
+
+        return successResponse('Successful', UserResource::make($user));
+
+
     }
 }
