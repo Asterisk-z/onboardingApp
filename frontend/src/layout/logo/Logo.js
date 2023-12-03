e18c8c161df75592c4cb5634e4b11f9c69c9cefe
slowmodev <williamsmomoh@gmail.com>
2023-12-03 11:59:29 +0100

    changed user dashboard page and replaced logo

--- frontend/src/layout/logo/Logo.js
+++ frontend/src/layout/logo/Logo.js
@@ -1,15 +1,19 @@
 import React from "react";
-import LogoLight2x from "../../images/logo2x.png";
-import LogoDark2x from "../../images/logo-dark2x.png";
-import LogoSmall from "../../images/logo-small.png";
+import Logomain from "../../images/fmdq/FMDQ-Logo.png"
+import LogoSmall from "../../images/fmdq/fmdqg_logo_cca-removebg-preview.png";
 import {Link} from "react-router-dom";
 
 const Logo = () => {
+  const styles = {
+    width: {
+      color: "200px",
+    },
+    
+  }
   return (
     <Link to={`${process.env.PUBLIC_URL}/`} className="logo-link">
-      <img className="logo-light logo-img" src={LogoLight2x} alt="logo" />
-      <img className="logo-dark logo-img" src={LogoDark2x} alt="logo" />
-      <img className="logo-small logo-img logo-img-small" src={LogoSmall} alt="logo" />
+      <img className={styles.width} src={Logomain} alt="logo" />
+      {/* <img className="logo-small logo-img logo-img-small" src={LogoSmall} alt="logo" /> */}
     </Link>
   );
 };
