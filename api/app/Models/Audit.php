31d2fcf81e3500d5771ff0fbce1c61d5c7fe70f4
EbuCodes <ebucodes@gmail.com>
2023-12-04 08:33:34 +0100

    Ebuka

--- api/app/Models/Audit.php
+++ api/app/Models/Audit.php
@@ -9,7 +9,7 @@ class Audit extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id'];
+    // protected $guarded = ['id'];
     protected $table = 'audit';
     public $timestamps = false;
     protected $guarded = [];
