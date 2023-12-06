65970ef9a5b620dd3f8b147868d22b0ba043fc8a
Olang Daniel <olangdan17@gmail.com>
2023-12-06 23:47:15 +0100

    working

--- frontend/src/main/pages/Complaint.js
+++ frontend/src/main/pages/Complaint.js
@@ -1,126 +1,115 @@
 import React, { useEffect, useState } from "react";
-import Head from "../layout/head/Head";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Content from "../layout/content/Content";
 import { useForm } from "react-hook-form";
 import { Modal, ModalHeader, ModalBody, ModalFooter, Card, Spinner} from "reactstrap";
-import { Block, BlockHead, BlockHeadContent, BlockTitle, Icon, Button, Row, Col, BlockBetween, RSelect } from "components/Component";
-import { loadAllComplaintTypes } from "../../redux/stores/complaints/complaintTypes";
-import { sendComplaint } from "../../redux/stores/complaints/complaint";
+import { Block, BlockHead, BlockHeadContent, BlockTitle, Icon, Button, Row, Col, BlockBetween, RSelect, BlockDes, BackTo, PreviewCard, ReactDataTable } from "components/Component";
+import { loadAllComplaintTypes } from "redux/stores/complaints/complaintTypes";
+import { sendComplaint, loadAllComplaints } from "redux/stores/complaints/complaint";
 
-import { orderData } from "components/table/TableData";
-import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Badge } from "reactstrap";
-
+import { DataTableData, dataTableColumns, dataTableColumns2, userData, orderData } from "components/table/TableData";
+import Content from "layout/content/Content";
+import Head from "layout/head/Head";
+import ComplaintTableUser from './ComplaintTableUser'
 
 const ComplainTable = () => {
-  const DropdownTrans = () => {
+    
+    const dispatch = useDispatch();
+    const complaints = useSelector((state) => state?.complaint?.list) || null;
+    useEffect(() => {
+        dispatch(loadAllComplaints());
+    }, [dispatch]);
+  
+    
+    const $complaints = complaints ? JSON.parse(complaints) : null;
+  
     return (
-      <UncontrolledDropdown>
-        <DropdownToggle tag="a" className="text-soft dropdown-toggle btn btn-icon btn-trigger">
-          <Icon name="more-h"></Icon>
-        </DropdownToggle>
-        <DropdownMenu end>
-          <ul className="link-list-plain">
-            <li>
-              <DropdownItem
-                tag="a"
-                href="#dropdownitem"
-                onClick={(ev) => {
-                  ev.preventDefault();
-                }}
-              >
-                View
-              </DropdownItem>
-            </li>
-            <li>
-              <DropdownItem
-                tag="a"
-                href="#dropdownitem"
-                onClick={(ev) => {
-                  ev.preventDefault();
-                }}
-              >
-                Invoice
-              </DropdownItem>
-            </li>
-            <li>
-              <DropdownItem
-                tag="a"
-                href="#dropdownitem"
-                onClick={(ev) => {
-                  ev.preventDefault();
-                }}
-              >
-                Print
-              </DropdownItem>
-            </li>
-          </ul>
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    );
-  };
-  return (
-    <table className="table table-orders">
-      <thead className="tb-odr-head">
-        <tr className="tb-odr-item">
-          <th className="tb-odr-info">
-            <span className="tb-odr-id">Ticket ID</span>
-            <span className="tb-odr-date d-none d-md-inline-block">Date Create</span>
-          </th>
-          <th className="tb-odr-amount">
-            <span className="tb-odr-total">Description</span>
-            <span className="tb-odr-status d-none d-md-inline-block">Status</span>
-          </th>
-          <th className="tb-odr-action">&nbsp;</th>
-        </tr>
-      </thead>
-      <tbody className="tb-odr-body">
-        {orderData.map((item) => {
-          return (
-            <tr className="tb-odr-item" key={item.id}>
-              <td className="tb-odr-info">
-                <span className="tb-odr-id">
-                  <a
-                    href="#id"
-                    onClick={(ev) => {
-                      ev.preventDefault();
-                    }}
-                  >
-                    {item.id}
-                  </a>
-                </span>
-                <span className="tb-odr-date">{item.date}</span>
-              </td>
-              <td className="tb-odr-amount">
-                <span className="tb-odr-total">
-                  <span className="amount">${item.amount}</span>
-                </span>
-                <span className="tb-odr-status">
-                  <Badge
-                    className="badge-dot"
-                    color={
-                      item.status === "Complete" ? "success" : item.status === "Pending" ? "warning" : "danger"
-                    }
-                  >
-                    {item.status}
-                  </Badge>
-                </span>
-              </td>
-              <td className="tb-odr-action">
-                <div className="tb-odr-btns d-none d-md-inline">
-                  <Button color="primary" className="btn-sm">
-                    View
-                  </Button>
-                </div>
-                <DropdownTrans />
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <React.Fragment>
+      <Content>
+
+
+        <Block size="xl">
+          <BlockHead>
+            <BlockHeadContent>
+              <BlockTitle tag="h4">Complaint History</BlockTitle>
+                <p>{complaints}</p>
+                {/* <DropdownTrans/> */}
+            </BlockHeadContent>
+          </BlockHead>
+
+          <PreviewCard>
+            {$complaints && <ComplaintTableUser data={$complaints} expandableRows pagination actions />}
+            
+            {/* <ComplaintTableUser data={DataTableData} columns={dataTableColumns} expandableRows pagination actions /> */}
+          </PreviewCard>
+        </Block>
+
+
+      </Content>
+    </React.Fragment>
   );
+
+  // return (
+  //   <table className="table table-orders">
+  //     <thead className="tb-odr-head">
+  //       <tr className="tb-odr-item">
+  //         <th className="tb-odr-info">
+  //           <span className="tb-odr-id">Ticket ID</span>
+  //           <span className="tb-odr-date d-none d-md-inline-block">Date Create</span>
+  //         </th>
+  //         <th className="tb-odr-amount">
+  //           <span className="tb-odr-total">Description</span>
+  //           <span className="tb-odr-status d-none d-md-inline-block">Status</span>
+  //         </th>
+  //         <th className="tb-odr-action">&nbsp;</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody className="tb-odr-body">
+  //       {orderData.map((item) => {
+  //         return (
+  //           <tr className="tb-odr-item" key={item.id}>
+  //             <td className="tb-odr-info">
+  //               <span className="tb-odr-id">
+  //                 <a
+  //                   href="#id"
+  //                   onClick={(ev) => {
+  //                     ev.preventDefault();
+  //                   }}
+  //                 >
+  //                   {item.id}
+  //                 </a>
+  //               </span>
+  //               <span className="tb-odr-date">{item.date}</span>
+  //             </td>
+  //             <td className="tb-odr-amount">
+  //               <span className="tb-odr-total">
+  //                 <span className="amount">${item.amount}</span>
+  //               </span>
+  //               <span className="tb-odr-status">
+  //                 <Badge
+  //                   className="badge-dot"
+  //                   color={
+  //                     item.status === "Complete" ? "success" : item.status === "Pending" ? "warning" : "danger"
+  //                   }
+  //                 >
+  //                   {item.status}
+  //                 </Badge>
+  //               </span>
+  //             </td>
+  //             <td className="tb-odr-action">
+                // <div className="tb-odr-btns d-none d-md-inline">
+                //   <Button color="primary" className="btn-sm">
+                //     View
+                //   </Button>
+                // </div>
+  //               <DropdownTrans />
+  //             </td>
+  //           </tr>
+  //         );
+  //       })}
+  //     </tbody>
+  //   </table>
+  // );
 };
 
 const Complaint = ({ drawer }) => {
@@ -131,7 +120,7 @@ const Complaint = ({ drawer }) => {
     const [complainFile, setComplainFile] = useState([]);
     const [sm, updateSm] = useState(false);
     const [modalForm, setModalForm] = useState(false);
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, resetField } = useForm();
     const complaintType = useSelector((state) => state?.complaintType?.list) || null;
 
     const toggleForm = () => setModalForm(!modalForm);
@@ -147,7 +136,7 @@ const Complaint = ({ drawer }) => {
         formData.append('complaint_type', values.complaint_type)
         formData.append('body', values.body)
         formData.append('document', complainFile)
-        console.log(complainFile)
+        
         try {
             setLoading(true);
             
@@ -158,6 +147,9 @@ const Complaint = ({ drawer }) => {
                   navigate(`${process.env.PUBLIC_URL}/complaint`);
                   setLoading(false);
                   setModalForm(!modalForm)
+                  resetField('complaint_type')
+                  resetField('body')
+                  resetField('document')
                 }, 1000);
             
             } else {
@@ -171,7 +163,7 @@ const Complaint = ({ drawer }) => {
     }; 
 
     const handleFileChange = (event) => {
-		setComplainFile(event.target.files[0]);
+		  setComplainFile(event.target.files[0]);
     };
     
 
@@ -260,11 +252,6 @@ const Complaint = ({ drawer }) => {
                     </ModalFooter>
                 </Modal>
                 <Block size="lg">
-                    <BlockHead>
-                        <BlockHeadContent>
-                            <BlockTitle tag="h4">Complains History</BlockTitle>
-                        </BlockHeadContent>
-                    </BlockHead>
                     <Card className="card-bordered card-preview">
                         <ComplainTable />
                     </Card>
---
+++ frontend/src/main/pages/ComplaintTableUser.js
@@ -0,0 +1,331 @@
+import React, { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import exportFromJSON from "export-from-json";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { Col, Modal, ModalBody, Row, Button, Dropdown, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import { DataTablePagination } from "components/Component";
+import moment from "moment";
+import Icon from "components/icon/Icon";
+
+const Export = ({ data }) => {
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    if (modal === true) {
+      setTimeout(() => setModal(false), 2000);
+    }
+  }, [modal]);
+
+  const fileName = "user-data";
+
+  const exportCSV = () => {
+    const exportType = exportFromJSON.types.csv;
+    exportFromJSON({ data, fileName, exportType });
+  };
+
+  const exportExcel = () => {
+    const exportType = exportFromJSON.types.xls;
+    exportFromJSON({ data, fileName, exportType });
+  };
+
+  const copyToClipboard = () => {
+    setModal(true);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="dt-export-buttons d-flex align-center">
+        <div className="dt-export-title d-none d-md-inline-block">Export</div>
+        <div className="dt-buttons btn-group flex-wrap">
+          <CopyToClipboard text={JSON.stringify(data)}>
+            <Button className="buttons-copy buttons-html5" onClick={() => copyToClipboard()}>
+              <span>Copy</span>
+            </Button>
+          </CopyToClipboard>{" "}
+          <button className="btn btn-secondary buttons-csv buttons-html5" type="button" onClick={() => exportCSV()}>
+            <span>CSV</span>
+          </button>{" "}
+          <button className="btn btn-secondary buttons-excel buttons-html5" type="button" onClick={() => exportExcel()}>
+            <span>Excel</span>
+          </button>{" "}
+        </div>
+      </div>
+      <Modal isOpen={modal} className="modal-dialog-centered text-center" size="sm">
+        <ModalBody className="text-center m-2">
+          <h5>Copied to clipboard</h5>
+        </ModalBody>
+        <div className="p-3 bg-light">
+          <div className="text-center">Copied {data.length} rows to clipboard</div>
+        </div>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+const ExpandableRowComponent = ({ data }) => {
+  return (
+    <ul className="dtr-details p-2 border-bottom ms-1">
+      <li className="d-block d-sm-none">
+        <span className="dtr-title">Company</span> <span className="dtr-data">{data.company}</span>
+      </li>
+      <li className="d-block d-sm-none">
+        <span className="dtr-title ">Gender</span> <span className="dtr-data">{data.gender}</span>
+      </li>
+      <li>
+        <span className="dtr-title">Start Date</span> <span className="dtr-data">{data.startDate}</span>
+      </li>
+      <li>
+        <span className="dtr-title">Salary</span> <span className="dtr-data">{data.salary}</span>
+      </li>
+    </ul>
+  );
+};
+
+const CustomCheckbox = React.forwardRef(({ onClick, ...rest }, ref) => (
+  <div className="custom-control custom-control-sm custom-checkbox notext">
+    <input
+      id={rest.name}
+      type="checkbox"
+      className="custom-control-input"
+      ref={ref}
+      onClick={onClick}
+      {...rest}
+    />
+    <label className="custom-control-label" htmlFor={rest.name} />
+  </div>
+));
+
+const DropdownExample = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggle = () => {setIsOpen(!isOpen)};
+
+  return (
+    <Dropdown isOpen={true} toggle={toggle}>
+      <DropdownToggle className="btn-action" color="primary">
+        <span>Dropdown Button</span>
+      </DropdownToggle>
+      <DropdownMenu>
+        <ul className="link-list-opt">
+          <li>
+            <DropdownItem
+              tag="a"
+              href="#links"
+              onClick={(ev) => ev.preventDefault()}
+            >
+              <span>Profile Settings</span>
+            </DropdownItem>
+          </li>
+          <li>
+            <DropdownItem
+              tag="a"
+              href="#links"
+              onClick={(ev) => ev.preventDefault()}
+            >
+              <span>Notifications</span>
+            </DropdownItem>
+          </li>
+          <li>
+            <DropdownItem
+              tag="a"
+              href="#links"
+              onClick={(ev) => ev.preventDefault()}
+            >
+              <span>Another Action</span>
+            </DropdownItem>
+          </li>
+          <li>
+            <DropdownItem
+              tag="a"
+              href="#links"
+              onClick={(ev) => ev.preventDefault()}
+            >
+              <span>Something else here</span>
+            </DropdownItem>
+          </li>
+        </ul>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+const DropdownTrans = () => {
+    return (
+                <UncontrolledDropdown direction="right">
+                  <div className="btn-group">
+                    <Button color="secondary">Action</Button>
+                    <DropdownToggle className="dropdown-toggle-split" color="secondary">
+                      <Icon name="chevron-right"></Icon>
+                    </DropdownToggle>
+                  </div>
+                  <DropdownMenu>
+                    <ul className="link-list-opt">
+                      <li>
+                        <DropdownItem tag="a" href="#links" onClick={(ev) => ev.preventDefault()}>
+                          <span>Profile Settings</span>
+                        </DropdownItem>
+                      </li>
+                      <li>
+                        <DropdownItem tag="a" href="#links" onClick={(ev) => ev.preventDefault()}>
+                          <span>Notifications</span>
+                        </DropdownItem>
+                      </li>
+                      <li>
+                        <DropdownItem tag="a" href="#links" onClick={(ev) => ev.preventDefault()}>
+                          <span>Another Action</span>
+                        </DropdownItem>
+                      </li>
+                      <li>
+                        <DropdownItem tag="a" href="#links" onClick={(ev) => ev.preventDefault()}>
+                          <span>Something else here</span>
+                        </DropdownItem>
+                      </li>
+                    </ul>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+
+    );
+  };
+
+const complainColumn = [
+      {
+        name: "Ticket ID",
+        selector: (row) => row.id,
+        sortable: true,
+      },
+      {
+        name: "Body",
+        selector: (row) => row.body,
+        sortable: true,
+        hide: 370,
+      },
+      {
+        name: "Status",
+        selector: (row) => row.status,
+        sortable: true,
+        hide: "sm",
+      },
+      {
+        name: "Date Created",
+        selector: (row) => moment(row.createdAt).format('MMM. DD, YYYY HH:mm'),
+        sortable: true,
+        hide: "md",
+      },
+      {
+        name: "Action",
+        selector: (row) => (<DropdownExample/>),
+        sortable: true,
+        hide: "md",
+      },
+  ];
+
+const ComplaintTableUser = ({ data, pagination, actions, className, selectableRows, expandableRows }) => {
+  const [tableData, setTableData] = useState(data);
+  const [searchText, setSearchText] = useState("");
+  const [rowsPerPageS, setRowsPerPage] = useState(10);
+  const [mobileView, setMobileView] = useState();
+
+  useEffect(() => {
+    let defaultData = tableData;
+    if (searchText !== "") {
+      defaultData = data.filter((item) => {
+        return item.name.toLowerCase().includes(searchText.toLowerCase());
+      });
+      setTableData(defaultData);
+    } else {
+      setTableData(data);
+    }
+  }, [searchText]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // function to change the design view under 1200 px
+  const viewChange = () => {
+    if (window.innerWidth < 960 && expandableRows) {
+      setMobileView(true);
+    } else {
+      setMobileView(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("load", viewChange);
+    window.addEventListener("resize", viewChange);
+    return () => {
+      window.removeEventListener("resize", viewChange);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div className={`dataTables_wrapper dt-bootstrap4 no-footer ${className ? className : ""}`}>
+      <Row className={`justify-between g-2 ${actions ? "with-export" : ""}`}>
+        <Col className="col-7 text-start" sm="4">
+          <div id="DataTables_Table_0_filter" className="dataTables_filter">
+            <label>
+              <input
+                type="search"
+                className="form-control form-control-sm"
+                placeholder="Search by name"
+                onChange={(ev) => setSearchText(ev.target.value)}
+              />
+            </label>
+          </div>
+        </Col>
+        <Col className="col-5 text-end" sm="8">
+          <div className="datatable-filter">
+            <div className="d-flex justify-content-end g-2">
+              {actions && <Export data={data} />}
+              <div className="dataTables_length" id="DataTables_Table_0_length">
+                <label>
+                  <span className="d-none d-sm-inline-block">Show</span>
+                  <div className="form-control-select">
+                    {" "}
+                    <select
+                      name="DataTables_Table_0_length"
+                      className="custom-select custom-select-sm form-control form-control-sm"
+                      onChange={(e) => setRowsPerPage(e.target.value)}
+                      value={rowsPerPageS}
+                    >
+                      <option value="10">10</option>
+                      <option value="25">25</option>
+                      <option value="40">40</option>
+                      <option value="50">50</option>
+                    </select>{" "}
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <DataTable
+        data={tableData}
+        columns={complainColumn}
+        className={className}
+        selectableRows={selectableRows}
+        selectableRowsComponent={CustomCheckbox}
+        expandableRowsComponent={ExpandableRowComponent}
+        expandableRows={mobileView}
+        noDataComponent={<div className="p-2">There are no records found</div>}
+        sortIcon={
+          <div>
+            <span>&darr;</span>
+            <span>&uarr;</span>
+          </div>
+        }
+        pagination={pagination}
+        paginationComponent={({ currentPage, rowsPerPage, rowCount, onChangePage, onChangeRowsPerPage }) => (
+          <DataTablePagination
+            customItemPerPage={rowsPerPageS}
+            itemPerPage={rowsPerPage}
+            totalItems={rowCount}
+            paginate={onChangePage}
+            currentPage={currentPage}
+            onChangeRowsPerPage={onChangeRowsPerPage}
+            setRowsPerPage={setRowsPerPage}
+          />
+        )}
+      ></DataTable>
+    </div>
+  );
+};
+
+export default ComplaintTableUser;
