82058c5bb9e1c016f2b21b98a78ee2971a672674
slowmodev <williamsmomoh@gmail.com>
2023-12-13 13:32:10 +0100

    temp table

--- frontend/src/main/pages/Homepage.js
+++ frontend/src/main/pages/Homepage.js
@@ -13,7 +13,120 @@ import {
   Col,
   BlockBetween,
 } from "components/Component";
+import { orderData } from "../../components/table/TableData";
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Badge } from "reactstrap";=
 import "../../App.css"
+
+
+export const OrderTable = () => {
+  const DropdownTrans = () => {
+    return (
+      <UncontrolledDropdown>
+        <DropdownToggle tag="a" className="text-soft dropdown-toggle btn btn-icon btn-trigger">
+          <Icon name="more-h"></Icon>
+        </DropdownToggle>
+        <DropdownMenu end>
+          <ul className="link-list-plain">
+            <li>
+              <DropdownItem
+                tag="a"
+                href="#dropdownitem"
+                onClick={(ev) => {
+                  ev.preventDefault();
+                }}
+              >
+                View
+              </DropdownItem>
+            </li>
+            <li>
+              <DropdownItem
+                tag="a"
+                href="#dropdownitem"
+                onClick={(ev) => {
+                  ev.preventDefault();
+                }}
+              >
+                Invoice
+              </DropdownItem>
+            </li>
+            <li>
+              <DropdownItem
+                tag="a"
+                href="#dropdownitem"
+                onClick={(ev) => {
+                  ev.preventDefault();
+                }}
+              >
+                Print
+              </DropdownItem>
+            </li>
+          </ul>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  };
+  return (
+    <table className="table table-orders">
+      <thead className="tb-odr-head">
+        <tr className="tb-odr-item">
+          <th className="tb-odr-info">
+            <span className="tb-odr-id">Order ID</span>
+            <span className="tb-odr-date d-none d-md-inline-block">Date</span>
+          </th>
+          <th className="tb-odr-amount">
+            <span className="tb-odr-total">Amount</span>
+            <span className="tb-odr-status d-none d-md-inline-block">Status</span>
+          </th>
+          <th className="tb-odr-action">&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody className="tb-odr-body">
+        {orderData.map((item) => {
+          return (
+            <tr className="tb-odr-item" key={item.id}>
+              <td className="tb-odr-info">
+                <span className="tb-odr-id">
+                  <a
+                    href="#id"
+                    onClick={(ev) => {
+                      ev.preventDefault();
+                    }}
+                  >
+                    {item.id}
+                  </a>
+                </span>
+                <span className="tb-odr-date">{item.date}</span>
+              </td>
+              <td className="tb-odr-amount">
+                <span className="tb-odr-total">
+                  <span className="amount">${item.amount}</span>
+                </span>
+                <span className="tb-odr-status">
+                  <Badge
+                    className="badge-dot"
+                    color={
+                      item.status === "Complete" ? "success" : item.status === "Pending" ? "warning" : "danger"
+                    }
+                  >
+                    {item.status}
+                  </Badge>
+                </span>
+              </td>
+              <td className="tb-odr-action">
+                <div className="tb-odr-btns d-none d-md-inline">
+                  <Button color="primary" className="btn-sm">
+                    View
+                  </Button>
+                </div>
+                <DropdownTrans />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 const Homepage = () => {
   const [sm, updateSm] = useState(false);
@@ -111,6 +224,20 @@ const Homepage = () => {
               <TopProducts />
             </Col> */}
           </Row>
+        </Block>
+        <Block size="lg">
+          <BlockHead>
+            <BlockHeadContent>
+              <BlockTitle tag="h4">Order History - With Action</BlockTitle>
+              <p>
+                The following table can be use for{" "}
+                <strong className="text-primary">order history, invoice listing</strong> related transaction.
+              </p>
+            </BlockHeadContent>
+          </BlockHead>
+          <Card className="card-bordered card-preview">
+            <OrderTable />
+          </Card>
         </Block>
       </Content>
     </React.Fragment>
